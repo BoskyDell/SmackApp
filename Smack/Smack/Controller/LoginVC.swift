027b200a26d16576349e83fcf8fe0718b7580ccd
Kevin Keefe <kevin@boskydell.tech>
2017-11-27 12:19:39 -0500

    Added Lesson 4 Login VC

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Kevin Keefe on 11/27/17.
+//  Copyright © 2017 Kevin Keefe. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    // Outlets -- none so far
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Contants.swift
@@ -0,0 +1,13 @@
+//
+//  Contants.swift
+//  Smack
+//
+//  Created by Kevin Keefe on 11/27/17.
+//  Copyright © 2017 Kevin Keefe. All rights reserved.
+//
+
+import Foundation
+
+// Seques
+let TO_LOGIN = "toLogin"
+
