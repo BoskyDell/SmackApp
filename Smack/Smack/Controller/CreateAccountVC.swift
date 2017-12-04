13c0ca0c775f10129e3dc3311d75340e8df44d6a
Kevin Keefe <kevin@boskydell.tech>
2017-12-04 11:13:46 -0500

    Lesson 10 created a user via Heroku and MongoDB

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    
+    @IBOutlet weak var usernameTxt: UILabel!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func createAccntPressed(_ sender: Any) {
+        
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        
+        guard let pass  = passTxt.text ,  passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("\(email) reistered User")
+            }
+        }
+    }
+   
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
+    
+    
     @IBAction func closedPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
---
+++ Smack/Smack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Kevin Keefe on 12/4/17.
+//  Copyright © 2017 Kevin Keefe. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+ 
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+}
