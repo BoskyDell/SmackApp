df3880ee09c2d7549b03bbc2013f6b4cf6bf914b
Kevin Keefe <kevin@boskydell.tech>
2017-12-05 16:22:11 -0500

    Added login user functionality

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -30,7 +30,12 @@ class CreateAccountVC: UIViewController {
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("\(email) reistered User")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("\(email) user logged in", AuthService.instance.authToken)
+                    }
+                    
+                })
             }
         }
     }
--- Smack/Smack/Services/AuthService.swift
+++ Smack/Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -58,7 +59,7 @@ class AuthService {
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             if response.result.error == nil {
                 completion(true)
             } else {
@@ -69,6 +70,51 @@ class AuthService {
         
     }
     
+    func loginUser ( email: String, password: String, completion: @escaping CompletionHandler ) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                
+                // Original JSON Parsing
+//                if let json = response.result.value as? Dictionary<String, Any> {
+//                    if let email = json["user" ] as? String {
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String {
+//                        self.authToken = token
+//                    }
+//                }
+
+                // Sifty JSON method
+                guard let data = response.data else { return }
+                
+                do {
+
+                    let json = try JSON(data: data)
+                    
+                    self.userEmail = json["user"].stringValue
+                    self.authToken = json["token"].stringValue
+                
+                    self.isLoggedIn = true
+                    completion(true)
+                } catch let error as NSError {
+                    completion(false)
+                    debugPrint(error as Any)
+                }
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
     
     
 }
