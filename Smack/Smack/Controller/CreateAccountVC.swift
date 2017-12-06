25ebc9e99ce6553e01709ed409bf48c29b3045eb
Kevin Keefe <kevin@boskydell.tech>
2017-12-06 09:14:35 -0500

    Added create user functionality

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -10,29 +10,37 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
-    
-    @IBOutlet weak var usernameTxt: UILabel!
+    // OUtlets
+    @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passTxt: UITextField!
     
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[ 0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
     @IBAction func createAccntPressed(_ sender: Any) {
         
-        guard let email = emailTxt.text , emailTxt.text != "" else { return }
-        
-        guard let pass  = passTxt.text ,  passTxt.text != "" else { return }
+        guard let email = emailTxt.text ,    emailTxt.text    != "" else { return }
+        guard let name  = usernameTxt.text,  usernameTxt.text != "" else { return }
+        guard let pass  = passTxt.text ,     passTxt.text     != "" else { return }
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("\(email) user logged in", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print( UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                     
                 })
